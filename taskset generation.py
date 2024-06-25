ad0a32be5d261a81cb423b1341d877013883cd9b
unknown <mohamadkhalafi.81@gmail.com>
2024-06-25 17:51:33 +0330

    a

--- taskset generation.py
+++ taskset generation.py
@@ -29,12 +29,12 @@ def generate_uunifastdiscard(nsets: int, u: float, n: int):
 
     return sets
 
+def trunc(x, p):
+    return int(x * 10 ** p) / float(10 ** p)
 
 def generate_tasksets(utilizations, periods, filename):
-    def trunc(x, p):
-        return int(x * 10 ** p) / float(10 ** p)
-
-    result = [[(trunc(ui * pi, 6), trunc(pi, 6)) for ui, pi in zip(us, ps)]
+    z = 3
+    result = [[(trunc(ui * pi, z), trunc(pi, z)) for ui, pi in zip(us, ps)]
               for us, ps in zip(utilizations, periods)]
 
     with open(filename, 'w', newline='') as csvfile:
@@ -62,7 +62,7 @@ def assign_preemption_level(taskset: list):
 def assign_resources(temp, res_num):
     for t in temp:
         executionTime = t[0]
-        res_points = [random.uniform(0, executionTime) for _ in range(0, 2*res_num)]
+        res_points = [trunc(random.uniform(0, executionTime), 3) for _ in range(0, 2*res_num)]
         res_points.sort()
         tuples_list = [(res_points[i], res_points[i + 1]) for i in range(0, len(res_points), 2)]
         random.shuffle(tuples_list)
@@ -87,6 +87,10 @@ def assign_tasks_to_cores(tasks, number_of_cores):
 
     return result
 
+def get_next_arriving_tasks_from_t(tasks, t):
+    remaining_times_to_arrival_from_t = [task.period - (t % task.period) for task in tasks]
+    min_remaining_time_i = remaining_times_to_arrival_from_t.index(min(remaining_times_to_arrival_from_t))
+    return tasks[min_remaining_time_i]
 
 class MyTask:
     task_count = 0
@@ -99,11 +103,52 @@ class MyTask:
         self.task_number = MyTask.task_count
         MyTask.task_count += 1
 
+        self.executed_time = 0
+        self.IsArrived = True
+    
+    def get_allocated_resources(self):
+        for res in self.resource_allocation_times:
+            if res[0] <= self.executed_time and res[1] > self.executed_time:
+                yield self.resource_allocation_times.index(res)
+    
 
-n = 100
-number_of_cores = 4
+    def get_next_releasing_resource_and_remaining_time_to_release(self):
+        if not self.IsArrived:
+            return
+        
+        allocated_resouce_indices = self.get_allocated_resources()
+        remaining_resource_allocating_times = \
+            [(self.resource_allocation_times[res_index][1]- self.executed_time) for res_index in allocated_resouce_indices] 
+        next_relesing_resource_index_in_list = \
+            remaining_resource_allocating_times.index(min(remaining_resource_allocating_times))
+        
+        next_releasing_resource = allocated_resouce_indices[next_relesing_resource_index_in_list]
+        time_to_release = remaining_resource_allocating_times[next_relesing_resource_index_in_list]
+        
+        return next_releasing_resource, time_to_release    
+    
+    def get_next_allocating_resource_and_remaining_time_to_allocate(self):
+        if not self.IsArrived:
+            return
+        
+        not_allocated_resouce_indices = filter(lambda i : self.executed_time < self.resource_allocation_times[i][0], \
+                                               range(0, len(self.resource_allocation_times)))
+        
+        remaining_time_to_allocate = \
+            [(self.executed_time - self.resource_allocation_times[res_index][0]) for res_index in not_allocated_resouce_indices] 
+        next_allocating_resource_index_in_list = \
+            remaining_time_to_allocate.index(min(remaining_time_to_allocate))
+        
+        next_allocating_resource = not_allocated_resouce_indices[next_allocating_resource_index_in_list]
+        time_to_allocate = remaining_time_to_allocate[next_allocating_resource_index_in_list]
+        
+        return next_allocating_resource, time_to_allocate
 
-temp = generate_tasksets(generate_uunifastdiscard(1, number_of_cores-2, 100),
+
+n = 5
+number_of_cores = 2
+
+temp = generate_tasksets(generate_uunifastdiscard(1, number_of_cores-1, n),
                          generate_random_periods_discrete(n, 1, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]), "a.csv")
 
 
@@ -119,3 +164,26 @@ tasks = list(createTasks(temp, resources))
 core_tasks = assign_tasks_to_cores(tasks, number_of_cores)
 
 print(resources)
+
+
+t = 0
+current_tasks = [None for _ in range(0, number_of_cores)]
+tasks_to_run = core_tasks.copy()
+
+for core_num in range(0, number_of_cores):
+    to_runs = tasks_to_run[core_num]
+    task_to_run = sorted(to_runs, key=lambda t: t.period)[0]
+    current_tasks[core_num] = task_to_run
+
+while True:
+    
+
+    
+
+
+
+
+
+
+
+
